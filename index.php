a0415123704a44eeb0c513e4e1bbefbaf04ce239
Matthew <85846402+Mateusz762@users.noreply.github.com>
2025-03-03 18:01:18 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Dziennik elektroniczny</title>
+</head>
+<body>
+<div class="container">
+    <div class="header">
+        <header>
+            <h2>Dziennik elektroniczny</h2>
+        </header>
+    </div>
+   <div class="signIn">Login:
+        <div class="fold">
+            <form action="log in.php method="post">
+                <input type="text" name="user" placecholder="user" required>
+                <input type="text" name="user" placecholder="password" required>
+                <input type="text" name="user" placecholder="checkbox" required>
+                <button>Zaloguj się</button>
+            </form>
+    
+        </div>
+   </div>
+</div>
+</body>
+</html>
